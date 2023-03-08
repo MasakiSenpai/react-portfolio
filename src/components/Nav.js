e55e720e40e6537433a48f7940de7f36baab1dd4
MasakiDesktop <mandij50@gmail.com>
2023-03-07 22:50:54 -0500

    tailwind config and made test nav

---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Nav() {
+    return (
+        <div>
+            <h1 className='bg-zinc-600 text-orange-400'>Nav Text</h1>
+        </div>
+    )
+}
