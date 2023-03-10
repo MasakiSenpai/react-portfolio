7601bf8a94314d95771eb277ac2dcb0543d2e074
MasakiDesktop <mandij50@gmail.com>
2023-03-10 15:23:13 -0500

    more style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,12 @@ import photo from '../images/snapchathead.jpg'
 
 export default function Home() {
     return (
-        <div className='h-100 flex flex-col'>
+        <div className='h-100 flex flex-col border border-4 border-rose bg-pale'>
             <div className='m-5 min-w-[170px] flex justify-center'>
                 <img className='h-96 border border-4 border-rose' src={photo} alt='Headshot'></img>
             </div>
             <div className='flex justify-center my-5 mb-10'> 
-                <div className='border border-4 border-rose bg-pale p-12 text-center max-w-[424px]'>
+                <div className='border border-4 border-rose bg-red-100 p-12 text-center max-w-[424px]'>
                     <h2> Hey, My name is Amanda </h2>
                     <p> Nice to meet you. I am a junior web devloper with the MERN stack who loves never leaving my house.
                         Recently graduated from Georgia Tech Full Stack Web Devlopemt BootCamp! ♪ \(´。`˵)／♪ </p>
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,17 +3,17 @@ import reactImg from '../images/react.svg'
 
 export default function Resume() {
     return (
-        <div className='h-100'>
-            <div className='bg-indigo-100'>
+        <div className='h-100 bg-indigo-100'>
+            <div className=''>
                 <img src={reactImg} alt='react'/>
             </div>
-            <div className='border border-4 border-tropical bg-indigo-100 p-12'>
-                <h2 className='text-3xl mb-3'> Front End </h2>
-                <p className='text-'> React, TailWindCss, Bootstrap, HTML, CSS </p>
+            <div className='border border-4 border-tropical bg-indigo-100 p-12 text-right'>
+                <h2 className='text-3xl mb-3 '> Front End </h2>
+                <p className=''> React, TailWindCss, Bootstrap, HTML, CSS </p>
             </div>
-            <div className='border border-4 border-tropical bg-indigo-100 p-12'>
+            <div className='border border-4 border-tropical bg-indigo-100 p-12 text-right'>
                 <h2 className='text-3xl mb-3'> Back End </h2>
-                <p className='text-'> Sql, Mongoose, NodeJs, Express  </p>
+                <p className=''> Sql, Mongoose, NodeJs, Express  </p>
             </div>
         </div>
     )
